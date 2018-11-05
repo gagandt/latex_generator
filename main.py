cbed659f17027105477bd465440f85a83457dfc4
GaganDeep Tomar <gagandeep280597@gmail.com>
2018-11-05 21:59:10 +0530

    added main

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+from confusion_matrix import print_conf_mat
+from performance_parameters import print_per_para
+
+
+
+M = [[1,2,3],[2,3,4],[3,4,5]]
+
+print_conf_mat(M)
+#print_per_para(M)
